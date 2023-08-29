b19079bbb05248cd3940b451a8291fd4f8d22075
emrgcmnn <e.gocmen1993@gmail.com>
2023-08-29 23:00:55 +0300

    aktarım hatası

--- src/Pages/Hakkımda.js
+++ src/Pages/Hakkımda.js
@@ -7,7 +7,7 @@ export default function Hakkımda () {
   return (<>
     <div class="container mx-auto px-4 py-8">
     <div class="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
-      <h1 class="text-3xl font-bold mb-4">I'm Emre Göçmen</h1>
+      <h1 class="text-3xl font-bold mb-4">Emre Göçmen</h1>
       <p class="text-gray-600 mb-4">I am Emre from Turkey. I live in Istanbul and  I'm 30 years old.</p>
       <p class="text-gray-600 mb-4">In 2016, I graduated from Düzce University, 
       Department of Tourism and Hotel Management. I
